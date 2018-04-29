151dcc0047fc5e66e351095526e92117a19ebb2a
nikni <nicola.tommasi.91@gmail.com>
2018-04-29 23:16:58 +0200

    Minor fixes to logger service

--- src/lib/src/services/logger.service.ts
+++ src/lib/src/services/logger.service.ts
@@ -6,25 +6,25 @@ export class Logger {
 
     constructor(private readonly optionService: JoyrideOptionsService) { }
 
-    debug(message?, data?) {
+    debug(message? : string, data? : any) {
         if (this.optionService.areLogsEnabled) {
             console.debug(message, data);
         }
     }
 
-    info(message?, data?) {
+    info(message? : string, data? : any) {
         if (this.optionService.areLogsEnabled) {
             console.info(message, data);
         }
     }
 
-    warn(message?, data?) {
+    warn(message? : string, data? : any) {
         if (this.optionService.areLogsEnabled) {
             console.warn(message, data);
         }
     }
 
-    error(message?, data?) {
+    error(message? : string, data? : any) {
         if (this.optionService.areLogsEnabled) {
             console.error(message, data);
         }
