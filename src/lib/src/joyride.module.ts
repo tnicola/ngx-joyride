856422876df7fda550c10f29f97fc2e82c0458c3
nikni <nicola.tommasi.91@gmail.com>
2018-05-01 10:18:32 +0200

    Removing empty line

--- src/lib/src/joyride.module.ts
+++ src/lib/src/joyride.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from "@angular/core";
-
 import { CommonModule } from "@angular/common";
 import { JoyrideDirective } from './directives/joyride.directive';
 import { JoyrideService } from './services/joyride.service';
