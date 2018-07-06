03487cdbe2eae0b0371df5798d042c6deed84233
nikni <nicola.tommasi.91@gmail.com>
2018-07-06 22:04:07 +0200

    Fixing unit tests

--- src/lib/src/test/fake/step-drawer-fake.service.ts
+++ src/lib/src/test/fake/step-drawer-fake.service.ts
@@ -3,4 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class StepDrawerServiceFake {
     draw: jasmine.Spy = jasmine.createSpy("draw");
+    remove: jasmine.Spy = jasmine.createSpy("remove");
+
 }
