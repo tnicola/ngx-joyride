1b9164d4c3a88bd8b5a0321716e50b30a8e928ea
nikni <nicola.tommasi.91@gmail.com>
2018-07-21 00:21:58 +0200

    BugFix: Do not restart the tour if it's already started

--- src/lib/src/services/joyride.service.ts
+++ src/lib/src/services/joyride.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@angular/core';
 import { JoyrideStepService } from "./joyride-step.service";
 import { JoyrideOptionsService } from '../services/joyride-options.service';
 import { JoyrideOptions } from '../models/joyride-options.class';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { JoyrideStepInfo } from '../models/joyride-step-info.class';
 
 @Injectable()
 export class JoyrideService {
+
+    private tourInProgress: boolean = false;
+
+    private tourSubscription$: Observable<JoyrideStepInfo>;
 
     constructor(
         private readonly stepService: JoyrideStepService,
@@ -14,10 +18,18 @@ export class JoyrideService {
     ) { }
 
     startTour(options?: JoyrideOptions): Observable<JoyrideStepInfo> {
-        if (options) {
-            this.optionsService.setOptions(options);
+        if (!this.tourInProgress) {
+            this.tourInProgress = true;
+            if (options) {
+                this.optionsService.setOptions(options);
+            }
+            this.tourSubscription$ = this.stepService.startTour().do(() => { }, () => { }, () => this.tourInProgress = false);
         }
-        return this.stepService.startTour();
+        return this.tourSubscription$;
+    }
+
+    isTourInProgress(): boolean {
+        return this.tourInProgress;
     }
 
 }
