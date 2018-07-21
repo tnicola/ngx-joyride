6eb10e4611f58d4722e52448535294a67a1c1cfe
nikni <nicola.tommasi.91@gmail.com>
2018-07-21 17:54:30 +0200

    Subscribing to startTour service

--- src/lib/src/services/joyride.service.ts
+++ src/lib/src/services/joyride.service.ts
@@ -3,14 +3,14 @@ import { JoyrideStepService } from "./joyride-step.service";
 import { JoyrideOptionsService } from './joyride-options.service';
 import { JoyrideOptions } from '../models/joyride-options.class';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/finally';
 import { JoyrideStepInfo } from '../models/joyride-step-info.class';
 
 @Injectable()
 export class JoyrideService {
 
     private tourInProgress: boolean = false;
-    private tourSubscription$: Observable<JoyrideStepInfo>;
+    private tour$: Observable<JoyrideStepInfo>;
 
     constructor(
         private readonly stepService: JoyrideStepService,
@@ -23,9 +23,10 @@ export class JoyrideService {
             if (options) {
                 this.optionsService.setOptions(options);
             }
-            this.tourSubscription$ = this.stepService.startTour().do(() => { }, () => { }, () => this.tourInProgress = false);
+            this.tour$ = this.stepService.startTour().finally(() => this.tourInProgress = false);
+            this.tour$.subscribe();
         }
-        return this.tourSubscription$;
+        return this.tour$;
     }
 
     isTourInProgress(): boolean {
