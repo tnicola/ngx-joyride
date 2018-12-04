09c9fe5bfe5964dfe00f1e1819ec2b3750dc1ceb
nikni <nicola.tommasi.91@gmail.com>
2018-12-04 22:38:40 +0100

    Adding unit tests for the DomService

--- src/lib/src/services/dom.service.spec.ts
+++ src/lib/src/services/dom.service.spec.ts
@@ -1,12 +1,38 @@
-import { TestBed } from "@angular/core/testing";
-import { DomRefService } from "./dom.service";
+import { TestBed } from '@angular/core/testing';
+import { DomRefService } from './dom.service';
+import { PLATFORM_ID } from '@angular/core';
 
-describe("DomRefService", () => {
+fdescribe('DomRefService', () => {
+    let platformId = '';
+    let domService: DomRefService;
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [
-                DomRefService
-            ]
+            providers: [DomRefService, { provide: PLATFORM_ID, useValue: platformId }]
         });
-    })
+    });
+
+    beforeEach(() => {
+        domService = TestBed.get(DomRefService);
+    });
+
+    describe('getNativeWindow()', () => {
+        it(`should return the real window if this.platformId is 'browser'`, () => {
+            domService = new DomRefService('browser');
+            expect(domService.getNativeWindow()).toEqual(window);
+        });
+        it(`should return the fake window if this.platformId is 'server'`, () => {
+            domService = new DomRefService('server');
+            expect(domService.getNativeWindow()).toEqual(<Window>{ document: { body: {}, documentElement: {} } });
+        });
+    });
+    describe('getNativeDocument()', () => {
+        it(`should return the real document if this.platformId is 'browser'`, () => {
+            domService = new DomRefService('browser');
+            expect(domService.getNativeDocument()).toEqual(document);
+        });
+        it(`should return the fake document if this.platformId is 'server'`, () => {
+            domService = new DomRefService('server');
+            expect(domService.getNativeDocument()).toEqual(<Document>{ body: {}, documentElement: {} });
+        });
+    });
 });
