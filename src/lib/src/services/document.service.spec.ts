cc441b9767563fe435252f7436b7476332ff54a0
nikni <nicola.tommasi.91@gmail.com>
2018-12-04 01:19:36 +0100

    Adding unit test for documentService

--- src/lib/src/services/document.service.spec.ts
+++ src/lib/src/services/document.service.spec.ts
@@ -1,63 +1,439 @@
-import { TestBed } from "@angular/core/testing";
-import { DocumentService } from "./document.service";
-import { DomRefService } from "./dom.service";
-import { DomRefServiceFake } from "../test/fake/dom-fake.service";
+import { TestBed } from '@angular/core/testing';
+import { DocumentService } from './document.service';
+import { DomRefService } from './dom.service';
+import { DomRefServiceFake } from '../test/fake/dom-fake.service';
+import { ElementRef } from '@angular/core';
+import { FakeElementRef } from '../test/fake/dom-elements-fake.class';
 
-describe("DocumentService", () => {
-
+describe('DocumentService', () => {
     let documentService: DocumentService;
+    let domRefService: DomRefServiceFake;
+    const getFakeDocument = compatMode => {
+        return {
+            compatMode,
+            documentElement: {
+                scrollTop: 10,
+                scrollLeft: 30
+            },
+            body: { scrollTop: 5, scrollLeft: 5 }
+        };
+    };
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [
-                DocumentService,
-                { provide: DomRefService, useClass: DomRefServiceFake }
-            ]
+            providers: [DocumentService, { provide: DomRefService, useClass: DomRefServiceFake }]
         });
     });
 
     beforeEach(() => {
-        // documentService = TestBed.get(DocumentService);
+        domRefService = TestBed.get(DomRefService);
     });
 
-    describe("getElementFixedTop", () => {
-        beforeEach(() => {
-            // spyOn(documentService, 'setDocumentHeight');
-        });
-        it("should return the relative top position", () => {
-            // FAKE di getNativeDocument, altrimenti il body va ad undefined
-            //
-            //
-            
-            /*let elementRef: ElementRef = {
-                nativeElement: {
-                    getBoundingClientRect: () => { return { top: 30 } }
+    describe('constructor', () => {
+        it('should set the initial height', () => {
+            domRefService.getNativeDocument.and.returnValue({
+                documentElement: {
+                    scrollHeight: 19,
+                    offsetHeight: 5,
+                    clientHeight: 50
+                },
+                body: {
+                    scrollHeight: 11,
+                    offsetHeight: 12,
+                    clientHeight: 3
                 }
-            };
-            console.log('Ciaone', elementRef.nativeElement.getBoundingClientRect());
-            let top = documentService.getElementFixedTop(elementRef);*/
+            });
 
-            expect(30).toBe(30);
+            documentService = TestBed.get(DocumentService);
+
+            expect(documentService.getDocumentHeight()).toBe(50);
         });
     });
+    describe('after service initialization', () => {
+        beforeEach(() => {
+            documentService = TestBed.get(DocumentService);
+        });
 
-    describe("getElementFixedLeft", () => {
+        describe('getElementFixedTop', () => {
+            beforeEach(() => {
+                spyOn(documentService, 'setDocumentHeight');
+            });
+            it('should return the relative top position', () => {
+                let elementRef: ElementRef = {
+                    nativeElement: {
+                        getBoundingClientRect: () => {
+                            return { top: 30 };
+                        }
+                    }
+                };
+                let top = documentService.getElementFixedTop(elementRef);
 
-    });
+                expect(top).toBe(30);
+            });
+        });
 
-    describe("getElementAbsoluteTop", () => {
+        describe('getElementFixedLeft', () => {
+            it('should return the position from left for a fixed element', () => {
+                const elementRef: ElementRef = {
+                    nativeElement: {
+                        getBoundingClientRect: () => {
+                            return { left: 123 };
+                        }
+                    }
+                };
+                const left = documentService.getElementFixedLeft(elementRef);
+                expect(left).toBe(123);
+            });
+        });
 
-    });
+        describe('getElementAbsoluteTop', () => {
+            describe('when pageXOffset is null', () => {
+                beforeEach(() => {
+                    domRefService.getNativeWindow.and.returnValue({ pageXOffset: null });
+                });
 
-    describe("getElementAbsoluteLeft", () => {
+                it('should return the absolute top position (documentElement scroll) for CSS1Compat browser', () => {
+                    const fakeDocument = getFakeDocument('CSS1Compat');
+                    domRefService.getNativeDocument.and.returnValue(fakeDocument);
+                    const elementRef: ElementRef = {
+                        nativeElement: {
+                            getBoundingClientRect: () => {
+                                return { top: 176 };
+                            }
+                        }
+                    };
 
-    });
+                    const top = documentService.getElementAbsoluteTop(elementRef);
 
-    describe("setDocumentHeight", () => {
+                    expect(top).toBe(186);
+                });
 
-    });
+                it('should return the absolute top position (body scroll) for BackCompat browser', () => {
+                    const fakeDocument = getFakeDocument('BackCompat');
+                    domRefService.getNativeDocument.and.returnValue(fakeDocument);
+                    const elementRef: ElementRef = {
+                        nativeElement: {
+                            getBoundingClientRect: () => {
+                                return { top: 123 };
+                            }
+                        }
+                    };
+                    const top = documentService.getElementAbsoluteTop(elementRef);
+                    expect(top).toBe(128);
+                });
 
-    describe("getDocumentHeight", () => {
+                it('should return the absolute top position (body scroll) if no compat mode is defined', () => {
+                    const fakeDocument = getFakeDocument(undefined);
+                    domRefService.getNativeDocument.and.returnValue(fakeDocument);
+                    const elementRef: ElementRef = {
+                        nativeElement: {
+                            getBoundingClientRect: () => {
+                                return { top: 123 };
+                            }
+                        }
+                    };
+                    const top = documentService.getElementAbsoluteTop(elementRef);
+                    expect(top).toBe(128);
+                });
+            });
+        });
+        describe('when pageXOffset is NOT null', () => {
+            beforeEach(() => {
+                domRefService.getNativeWindow.and.returnValue({ pageXOffset: 5, pageYOffset: 15 });
+            });
 
+            it('should return the getBoundingClientRect.top + pageYOffset for CSS1Compat browser', () => {
+                const fakeDocument = getFakeDocument('CSS1Compat');
+                domRefService.getNativeDocument.and.returnValue(fakeDocument);
+                const elementRef: ElementRef = {
+                    nativeElement: {
+                        getBoundingClientRect: () => {
+                            return { top: 15 };
+                        }
+                    }
+                };
+
+                const top = documentService.getElementAbsoluteTop(elementRef);
+
+                expect(top).toBe(30);
+            });
+
+            it('should return the getBoundingClientRect.top +  pageYOffset for BackCompat browser', () => {
+                const fakeDocument = getFakeDocument('BackCompat');
+                domRefService.getNativeDocument.and.returnValue(fakeDocument);
+                const elementRef: ElementRef = {
+                    nativeElement: {
+                        getBoundingClientRect: () => {
+                            return { top: 50 };
+                        }
+                    }
+                };
+                const top = documentService.getElementAbsoluteTop(elementRef);
+                expect(top).toBe(65);
+            });
+        });
+
+        describe('getElementAbsoluteLeft', () => {
+            describe('when pageXOffset is null', () => {
+                beforeEach(() => {
+                    domRefService.getNativeWindow.and.returnValue({ pageXOffset: null });
+                });
+
+                it('should return the absolute left position (documentElement scroll left + boundingClientLeft) for CSS1Compat browser', () => {
+                    const fakeDocument = getFakeDocument('CSS1Compat');
+                    domRefService.getNativeDocument.and.returnValue(fakeDocument);
+                    const elementRef: ElementRef = {
+                        nativeElement: {
+                            getBoundingClientRect: () => {
+                                return { left: 20 };
+                            }
+                        }
+                    };
+
+                    const left = documentService.getElementAbsoluteLeft(elementRef);
+
+                    expect(left).toBe(50);
+                });
+
+                it('should return the absolute left position (body scroll left + boundingClientLeft) for BackCompat browser', () => {
+                    const fakeDocument = getFakeDocument('BackCompat');
+                    domRefService.getNativeDocument.and.returnValue(fakeDocument);
+                    const elementRef: ElementRef = {
+                        nativeElement: {
+                            getBoundingClientRect: () => {
+                                return { left: 20 };
+                            }
+                        }
+                    };
+                    const left = documentService.getElementAbsoluteLeft(elementRef);
+                    expect(left).toBe(25);
+                });
+
+                it('should return the absolute top position ((body scroll left + boundingClientLeft) if no compat mode is defined', () => {
+                    const fakeDocument = getFakeDocument(undefined);
+                    domRefService.getNativeDocument.and.returnValue(fakeDocument);
+                    const elementRef: ElementRef = {
+                        nativeElement: {
+                            getBoundingClientRect: () => {
+                                return { left: 20 };
+                            }
+                        }
+                    };
+                    const left = documentService.getElementAbsoluteLeft(elementRef);
+                    expect(left).toBe(25);
+                });
+            });
+        });
+        describe('when pageXOffset is NOT null', () => {
+            beforeEach(() => {
+                domRefService.getNativeWindow.and.returnValue({ pageXOffset: 3, pageYOffset: 15 });
+            });
+
+            it('should return getBoundingClientRect.left + pageXOffset for CSS1Compat browser', () => {
+                const fakeDocument = getFakeDocument('CSS1Compat');
+                domRefService.getNativeDocument.and.returnValue(fakeDocument);
+                const elementRef: ElementRef = {
+                    nativeElement: {
+                        getBoundingClientRect: () => {
+                            return { left: 15 };
+                        }
+                    }
+                };
+
+                const left = documentService.getElementAbsoluteLeft(elementRef);
+
+                expect(left).toBe(18);
+            });
+
+            it('should return getBoundingClientRect.left + pageXOffset for BackCompat browser', () => {
+                const fakeDocument = getFakeDocument('BackCompat');
+                domRefService.getNativeDocument.and.returnValue(fakeDocument);
+                const elementRef: ElementRef = {
+                    nativeElement: {
+                        getBoundingClientRect: () => {
+                            return { left: 50 };
+                        }
+                    }
+                };
+                const left = documentService.getElementAbsoluteLeft(elementRef);
+                expect(left).toBe(53);
+            });
+        });
+
+        describe('setDocumentHeight', () => {
+            it(`should set the value of documentElement.scrollHeight`, () => {
+                domRefService.getNativeDocument.and.returnValue({
+                    documentElement: {
+                        scrollHeight: 19,
+                        offsetHeight: 5,
+                        clientHeight: 1
+                    },
+                    body: {
+                        scrollHeight: 11,
+                        offsetHeight: 12,
+                        clientHeight: 3
+                    }
+                });
+                documentService.setDocumentHeight();
+
+                expect(documentService.getDocumentHeight()).toBe(19);
+            });
+        });
+        describe('setDocumentHeight', () => {
+            it(`should set the value of documentElement.offsetHeight`, () => {
+                domRefService.getNativeDocument.and.returnValue({
+                    documentElement: {
+                        scrollHeight: 19,
+                        offsetHeight: 20,
+                        clientHeight: 1
+                    },
+                    body: {
+                        scrollHeight: 11,
+                        offsetHeight: 12,
+                        clientHeight: 3
+                    }
+                });
+
+                documentService.setDocumentHeight();
+
+                expect(documentService.getDocumentHeight()).toBe(20);
+            });
+        });
+        describe('setDocumentHeight', () => {
+            it(`should set the value of documentElement.clientHeight`, () => {
+                domRefService.getNativeDocument.and.returnValue({
+                    documentElement: {
+                        scrollHeight: 19,
+                        offsetHeight: 5,
+                        clientHeight: 21
+                    },
+                    body: {
+                        scrollHeight: 11,
+                        offsetHeight: 12,
+                        clientHeight: 3
+                    }
+                });
+
+                documentService.setDocumentHeight();
+
+                expect(documentService.getDocumentHeight()).toBe(21);
+            });
+        });
+        describe('setDocumentHeight', () => {
+            it(`should set the value of documentElement.body.scrollHeight`, () => {
+                domRefService.getNativeDocument.and.returnValue({
+                    documentElement: {
+                        scrollHeight: 19,
+                        offsetHeight: 5,
+                        clientHeight: 1
+                    },
+                    body: {
+                        scrollHeight: 22,
+                        offsetHeight: 12,
+                        clientHeight: 3
+                    }
+                });
+
+                documentService.setDocumentHeight();
+
+                expect(documentService.getDocumentHeight()).toBe(22);
+            });
+        });
+        describe('setDocumentHeight', () => {
+            it(`should set the value of documentElement.body.offsetHeight`, () => {
+                domRefService.getNativeDocument.and.returnValue({
+                    documentElement: {
+                        scrollHeight: 19,
+                        offsetHeight: 5,
+                        clientHeight: 1
+                    },
+                    body: {
+                        scrollHeight: 11,
+                        offsetHeight: 23,
+                        clientHeight: 3
+                    }
+                });
+
+                documentService.setDocumentHeight();
+
+                expect(documentService.getDocumentHeight()).toBe(23);
+            });
+        });
+        describe('setDocumentHeight', () => {
+            it(`should set the value of documentElement.body.clientHeight`, () => {
+                domRefService.getNativeDocument.and.returnValue({
+                    documentElement: {
+                        scrollHeight: 19,
+                        offsetHeight: 5,
+                        clientHeight: 1
+                    },
+                    body: {
+                        scrollHeight: 11,
+                        offsetHeight: 12,
+                        clientHeight: 24
+                    }
+                });
+
+                documentService.setDocumentHeight();
+
+                expect(documentService.getDocumentHeight()).toBe(24);
+            });
+        });
+
+        describe('getFirstScrollableParent', () => {
+            beforeEach(() => {
+                domRefService.getNativeDocument.and.returnValue({ body: { theBody: 2 } });
+                domRefService.getNativeWindow.and.returnValue({
+                    getComputedStyle: (node, val) => {
+                        return {
+                            getPropertyValue: prop => {
+                                return node.style[prop];
+                            }
+                        };
+                    }
+                });
+            });
+            it('should return the first node in the ancestor tree that has overflow set to one of auto|scroll|overlay', () => {
+                const element = new FakeElementRef();
+                element.nativeElement = {
+                    style: { overflow: 'auto' }
+                };
+                const node = documentService.getFirstScrollableParent(element.nativeElement);
+                expect(node).toBe(element.nativeElement);
+            });
+            it('should return the parent if the parent is the first scrollable', () => {
+                const element = new FakeElementRef();
+                element.nativeElement = {
+                    style: { height: '12px' },
+                    parentNode: { style: { overflow: 'scroll' } }
+                };
+                const node = documentService.getFirstScrollableParent(element.nativeElement);
+                expect(node).toBe(element.nativeElement.parentNode);
+            });
+            it('should return the 2nd parent if the 2nd parent is the first scrollable', () => {
+                const element = new FakeElementRef();
+                element.nativeElement = {
+                    style: { height: '12px' },
+                    parentNode: {
+                        style: { width: '20px' },
+                        parentNode: { style: { overflow: 'auto' } }
+                    }
+                };
+                const node = documentService.getFirstScrollableParent(element.nativeElement);
+                expect(node).toBe(element.nativeElement.parentNode.parentNode);
+            });
+            it('should return the body if none of the', () => {
+                const element = new FakeElementRef();
+                element.nativeElement = {
+                    style: { height: '12px' },
+                    parentNode: {
+                        style: { width: '20px' },
+                        parentNode: { style: { display: 'flex' } }
+                    }
+                };
+                const node = documentService.getFirstScrollableParent(element.nativeElement);
+                expect(node).toEqual(jasmine.objectContaining({ theBody: 2 }));
+            });
+        });
     });
 });
--- src/lib/src/services/document.service.ts
+++ src/lib/src/services/document.service.ts
@@ -1,14 +1,12 @@
-import { Injectable, ElementRef } from "@angular/core";
-import { DomRefService } from "./dom.service";
+import { Injectable, ElementRef } from '@angular/core';
+import { DomRefService } from './dom.service';
 
 @Injectable()
 export class DocumentService {
     private documentHeight: number;
-    private windowRef: Window;
 
     constructor(private readonly DOMService: DomRefService) {
         this.setDocumentHeight();
-        this.windowRef = this.DOMService.getNativeWindow();
     }
 
     getElementFixedTop(elementRef: ElementRef) {
@@ -21,18 +19,12 @@ export class DocumentService {
 
     getElementAbsoluteTop(elementRef: ElementRef) {
         var scrollOffsets = this.getScrollOffsets();
-        return (
-            elementRef.nativeElement.getBoundingClientRect().top +
-            scrollOffsets.y
-        );
+        return elementRef.nativeElement.getBoundingClientRect().top + scrollOffsets.y;
     }
 
     getElementAbsoluteLeft(elementRef: ElementRef) {
         var scrollOffsets = this.getScrollOffsets();
-        return (
-            elementRef.nativeElement.getBoundingClientRect().left +
-            scrollOffsets.x
-        );
+        return elementRef.nativeElement.getBoundingClientRect().left + scrollOffsets.x;
     }
 
     setDocumentHeight() {
@@ -47,18 +39,15 @@ export class DocumentService {
         const regex = /(auto|scroll|overlay)/;
 
         const style = (node: any, prop: any) =>
-            this.windowRef.getComputedStyle(node, null).getPropertyValue(prop);
+            this.DOMService.getNativeWindow()
+                .getComputedStyle(node, null)
+                .getPropertyValue(prop);
 
-        const scroll = (node: any) =>
-            regex.test(
-                style(node, "overflow") +
-                    style(node, "overflow-y") +
-                    style(node, "overflow-x")
-            );
+        const scroll = (node: any) => regex.test(style(node, 'overflow') + style(node, 'overflow-y') + style(node, 'overflow-x'));
 
         const scrollparent = (node: any): any => {
-            return !node || node === document.body
-                ? document.body
+            return !node || node === this.DOMService.getNativeDocument().body
+                ? this.DOMService.getNativeDocument().body
                 : scroll(node)
                 ? node
                 : scrollparent(node.parentNode);
@@ -68,31 +57,32 @@ export class DocumentService {
     }
 
     private calculateDocumentHeight() {
-        var doc = this.DOMService.getNativeDocument();
+        const documentRef = this.DOMService.getNativeDocument();
         return Math.max(
-            doc.body.scrollHeight,
-            doc.documentElement.scrollHeight,
-            doc.body.offsetHeight,
-            doc.documentElement.offsetHeight,
-            doc.body.clientHeight,
-            doc.documentElement.clientHeight
+            documentRef.body.scrollHeight,
+            documentRef.documentElement.scrollHeight,
+            documentRef.body.offsetHeight,
+            documentRef.documentElement.offsetHeight,
+            documentRef.body.clientHeight,
+            documentRef.documentElement.clientHeight
         );
     }
 
     private getScrollOffsets() {
-        var w = this.DOMService.getNativeWindow();
+        const winReference = this.DOMService.getNativeWindow();
+        const docReference = this.DOMService.getNativeDocument();
 
         // This works for all browsers except IE versions 8 and before
-        if (w.pageXOffset != null)
-            return { x: w.pageXOffset, y: w.pageYOffset };
+        if (winReference.pageXOffset != null) return { x: winReference.pageXOffset, y: winReference.pageYOffset };
+
         // For IE (or any browser) in Standards mode
-        var d = w.document;
-        if (document.compatMode == "CSS1Compat")
+        if (docReference.compatMode == 'CSS1Compat')
             return {
-                x: d.documentElement.scrollLeft,
-                y: d.documentElement.scrollTop
+                x: docReference.documentElement.scrollLeft,
+                y: docReference.documentElement.scrollTop
             };
+
         // For browsers in Quirks mode
-        return { x: d.body.scrollLeft, y: d.body.scrollTop };
+        return { x: docReference.body.scrollLeft, y: docReference.body.scrollTop };
     }
 }
