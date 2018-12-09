58f89d989a5223f60669e00d304de07c6e45a332
nikni <nicola.tommasi.91@gmail.com>
2018-12-09 23:05:14 +0100

    Adding an e2e test

--- cypress/integration/actions.spec.js
+++ cypress/integration/actions.spec.js
@@ -1,11 +1,23 @@
 /// <reference types="Cypress" />
 
 context('Actions', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:8080')
-  })
+    beforeEach(() => {
+        cy.visit('http://localhost:8080');
+    });
 
-  it.only('smoke test', () => {
-    cy.get('button').should('exist');
-  });
-})
+    it('should find the start button', () => {
+        cy.get('.app__start-button').should('exist');
+    });
+
+    it('should complete the tour with all the steps', () => {
+        cy.get('.app__start-button').click();
+        cy.get('.joyride-step__container').should('be.visible');
+
+        for (let i = 0; i < 10; i++) {
+            cy.get('.joyride-step__next-container').click();
+            cy.get('.joyride-step__container').should('be.visible');
+        }
+        
+        cy.get('.joyride-step__done-container').click();
+    });
+});
