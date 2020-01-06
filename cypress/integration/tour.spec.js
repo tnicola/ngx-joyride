4ce05cd4a346a13b12c5f3cdf1165134af5b33f2
nikni <nicola.tommasi.91@gmail.com>
2020-01-06 18:51:40 +0100

    Adding cypress to CI

---
+++ cypress/integration/tour.spec.js
@@ -0,0 +1,21 @@
+context('Actions', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:4500');
+    });
+
+    it('should find the start button', () => {
+        cy.get('.app__start-button').should('exist');
+    });
+
+    it('should complete the tour with all the steps', () => {
+        cy.get('.app__start-button').click();
+        cy.get('.joyride-step__container').should('be.visible');
+
+        for (let i = 0; i < 10; i++) {
+            cy.get('.joyride-step__next-container').click();
+            cy.get('.joyride-step__container').should('be.visible');
+        }
+
+        cy.get('.joyride-step__done-container').click();
+    });
+});
